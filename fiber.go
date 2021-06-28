2ce40f06d98b240b8e68b0316f3bb3c7cfd9aa95
Michael McQuade <michael@giraffesyo.io>
2021-06-28 00:22:09 -0500

    Add rect and area method

--- fiber.go
+++ fiber.go
@@ -1,12 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Rect struct {
+	length, width int
+}
+
+func (r Rect) Area() int {
+	return r.length * r.width
+}
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Get("/rect/:length/:width", func(c *fiber.Ctx) error {
+		length, _ := strconv.ParseInt(c.Params("length"), 10, 32)
+		width, _ := strconv.ParseInt(c.Params("width"), 10, 32)
+		r := Rect{int(length), int(width)}
+
+		area := fmt.Sprintf("%d", r.Area())
+
+		return c.SendString(area)
 	})
 
 	app.Listen(":3000")
